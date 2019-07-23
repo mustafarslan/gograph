9998935a12ebaaada3aeb65e454942ea75b4f9a9
Mustafa Arslan <mustafarslan@users.noreply.github.com>
2019-07-23 19:14:08 +0300

    Update main.go

--- main.go
+++ main.go
@@ -23,36 +23,44 @@ var jobs = []Job{
 	Job{9, 50, []int{2, 4, 6}},
 }
 
-var visited []int
 
-func dfs(ID int, job Job) {
+func dfs(ID int, job Job, list_visited []int, t_map map[int]int) []int{
 
-	index := sort.SearchInts(visited, ID)
-	if index < len(visited) && visited[index] == ID {
-		// found
-	} else {
-		visited = append(visited, ID)
-		for _, item := range job.childJobIDs {
-			pos := sort.Search(len(jobs), func(i int) bool { return jobs[i].ID >= item })
-			dfs(item, jobs[pos])
-		}
-	}
+    index := sort.SearchInts(list_visited, ID)
+    
+    if index < len(list_visited) && list_visited[index] == ID {
+        // found 
+    } else {
+
+        list_visited = append(list_visited, ID)    
+        for _, item := range job.childJobIDs {
+            list_visited = dfs(item, jobs[t_map[ID]], list_visited, t_map)
+        }
+    }
+
+    return list_visited
 }
 
-func totalExecutionTime(ID int, jobs []Job) int {
 
+func totalExecutionTime(ID int, jobs []Job) int {
+    
+    var temp_visited []int
+    temp_map := make(map[int]int)
+    
 	sort.Slice(jobs, func(i, j int) bool {
 		return jobs[i].ID <= jobs[j].ID
 	})
 
-	pos := sort.Search(len(jobs), func(i int) bool { return jobs[i].ID >= ID })
-	dfs(ID, jobs[pos])
-
+    for index, value := range jobs {
+        temp_map[value.ID] = index
+    }
+    
+	temp_visited = dfs(ID, jobs[temp_map[ID]], temp_visited, temp_map)
+    
 	sum := 0
-
-	for _, item := range visited {
-		pos := sort.Search(len(jobs), func(i int) bool { return jobs[i].ID >= item })
-		sum = sum + jobs[pos].jobTime
+  
+	for _, item := range temp_visited {
+		sum = sum + jobs[temp_map[item]].jobTime
 	}
 	return sum
 
@@ -60,6 +68,8 @@ func totalExecutionTime(ID int, jobs []Job) int {
 
 func main() {
 
-	fmt.Printf("Result is: %d", totalExecutionTime(1, jobs))
+    
+	fmt.Printf("Result is: %d\n", totalExecutionTime(1, jobs)) // 120
+	fmt.Printf("Result is: %d\n", totalExecutionTime(2, jobs)) // 30
 
 }
